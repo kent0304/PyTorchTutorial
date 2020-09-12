97f12662c761dcb45075d0b52727f0f486513829
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-09-12 18:44:12 +0900

    Make livedoor news data to pandas dataset

---
+++ classification/prepare_data.py
@@ -0,0 +1,20 @@
+import os 
+from glob import glob 
+import pandas as pd 
+import linecache
+
+categories = [name for name in os.listdir("../data/text") if os.path.isdir('../data/text/' + name)]
+
+datasets = pd.DataFrame(columns=["title", "category"])
+
+for cat in categories:
+    path = "../data/text/" + cat + "/*.txt"
+    files = glob(path)
+    for file in files:
+        title = linecache.getline(file,3)
+        row = pd.Series([title, cat], index=datasets.columns)
+        datasets = datasets.append(row, ignore_index=True)
+
+datasets = datasets.sample(frac=1, random_state=0).reset_index(drop=True)
+
+print(datasets.head())
